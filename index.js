bbef9c64201871dac7c617dae5dea912c17d67ea
Enes koç <32553624+eneskocc@users.noreply.github.com>
2019-11-11 17:38:56 +0300

    update

--- index.js
+++ index.js
@@ -17,31 +17,58 @@ function coz() {
         }else{
             x1=k;
         }
+
         iterasyon++;
-        addDiv(bir,iki,k,x1,x2,degerkok,iterasyon);
+        
+        if(iterasyon>3){
+            addDiv2(x1,x2,iterasyon);
+        }else{
+            addDiv(bir,iki,k,x1,x2,degerkok,iterasyon);
+        }
+       
         if((x2-x1)<0.000001){
             bitir(x1,x2);
             break;
         }
-    }
-    
-    
-    
+    }   
 }
+
+
+
+function addDiv2(x1,x2,iterasyon) {
+    var btn = document.createElement("DIV"); 
+    btn.innerHTML =""+iterasyon+". iterasyon"+"<br>"+"<br>"+"1.kök="+x1+"<br>"+"1.kök="+x1+"<br>";   
+    var typ = document.createAttribute("class");
+    typ.value = "d-flex bg-info justify-content-center align-items-center text-center rounded-pill text-white mt-3 p-3";
+    btn.attributes.setNamedItem(typ);               
+    document.getElementById("a").appendChild(btn);
+}
+
+
+
 function bitir(x1,x2) {
     var btn = document.createElement("DIV"); 
     btn.innerHTML ="Köklerimiz "+"<br>"+"1.kök="+x1+"<br>"+"1.kök="+x1+"<br>";   
     var typ = document.createAttribute("class");
-    typ.value = "d-flex bg-warning justify-content-center align-items-center text-center rounded-pill text-white mt-3";
+    typ.value = "d-flex bg-danger justify-content-center align-items-center text-center rounded-pill text-white mt-3 p-3";
     btn.attributes.setNamedItem(typ);               
     document.getElementById("a").appendChild(btn);
+
+    var btn1 = document.createElement("BUTTON");
+    btn1.innerHTML="RESET";
+    var typ1 = document.createAttribute("class");
+    typ1.value = "btn btn-outline-warning btn-lg btn-block rounded-pill mt-5";
+    btn1.attributes.setNamedItem(typ1); 
+    document.getElementById("a").appendChild(btn1);
 }
+
+
 
 function addDiv(params1,params2,k,x1,x2,degerkok,iterasyon) {
     var btn = document.createElement("DIV"); 
     btn.innerHTML = ""+iterasyon+". iterasyon"+"<br>"+"f("+x1+")="+params1+"<br>"+"f("+x2+")="+params2+"<br>"+"yeni kökümüz="+k+"<br>"+"f("+k+")="+degerkok;   
     var typ = document.createAttribute("class");
-    typ.value = "d-flex bg-info justify-content-center align-items-center text-center rounded-pill text-white mt-3";
+    typ.value = "d-flex bg-info justify-content-center align-items-center text-center rounded-pill text-white mt-3 p-3";
     btn.attributes.setNamedItem(typ);               
     document.getElementById("a").appendChild(btn);
 }
@@ -54,4 +81,9 @@ function hesapla(denklem,x) {
 
 function newKok(x1,x2) {
     return (x1+x2)/2;
+}
+
+
+function reset() {
+    document.location.reload(true);
 }
