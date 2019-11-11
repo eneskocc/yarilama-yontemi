f94c34163c44e00a60fd0ce470ebe6f70533ee5a
Enes koç <32553624+eneskocc@users.noreply.github.com>
2019-11-11 16:36:23 +0300

    finish

--- index.js
+++ index.js
@@ -6,16 +6,32 @@ function coz() {
     var denklemm=String(denklem.value);
     var x1=Number(aralik1.value);
     var x2=Number(aralik2.value);
-    var bir=hesapla(denklemm,x1);
-    var iki=hesapla(denklemm,x2);
-    var k=newKok(x1,x2);
-    addDiv(bir,iki,k,x1,x2);
+    var iterasyon=0;
+    while (true) {
+        var bir=hesapla(denklemm,x1);
+        var iki=hesapla(denklemm,x2);
+        var k=newKok(x1,x2);
+        var degerkok=hesapla(denklemm,k);
+        if(bir<0 && degerkok>0){
+            x2=k;
+        }else{
+            x1=k;
+        }
+        iterasyon++;
+        addDiv(bir,iki,k,x1,x2,degerkok,iterasyon);
+        if((x2-x1)<0.000001){
+            break;
+        }
+    }
+    
+    
+    
 }
 
 
-function addDiv(params1,params2,k,x1,x2) {
+function addDiv(params1,params2,k,x1,x2,degerkok,iterasyon) {
     var btn = document.createElement("DIV"); 
-    btn.innerHTML = "f("+x1+")="+params1+"<br>"+"f("+x2+")="+params2+"<br>"+"yeni kökümüz="+k;   
+    btn.innerHTML = ""+iterasyon+". iterasyon"+"<br>"+"f("+x1+")="+params1+"<br>"+"f("+x2+")="+params2+"<br>"+"yeni kökümüz="+k+"<br>"+"f("+k+")="+degerkok;   
     var typ = document.createAttribute("class");
     typ.value = "d-flex bg-info justify-content-center align-items-center text-center rounded-pill text-white mt-3";
     btn.attributes.setNamedItem(typ);               
